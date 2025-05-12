3a683415c210e3bdab66e9096b3f0a6f7afe2193
zeeshanhanif <zeeshanhanif@gmail.com>
2025-05-12 23:42:50 +0500

    Added tailwind config

---
+++ 04_chatbot_fastapi_websocket_nextjs/frontend/tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config; 
